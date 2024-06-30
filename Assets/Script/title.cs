ce7627379ba89883a02450b3bc75e450e866f248
kota.tonami <gintama.saicoo@gmail.com>
2024-07-01 02:39:25 +0900

    サンプルで作成した2Dゲームを追加

---
+++ Assets/Script/title.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class title : MonoBehaviour
+{
+    private bool firstPush = false;
+
+    public void PressStart()
+    {
+        Debug.Log("Press Satrt");
+        if (!firstPush)
+        {
+            Debug.Log("Go next");
+        }
+        
+    }
+}
